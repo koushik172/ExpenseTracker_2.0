0824a1228d9f06059dd7505429a1bfa5e3f1a17f
koushik172 <koushikrajbanshi14@gmail.com>
2023-12-01 23:54:11 +0530

    Create login page for user

--- expense_tracker_frontend/src/App.jsx
+++ expense_tracker_frontend/src/App.jsx
@@ -4,17 +4,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import your page components
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signUp" element={<SignupPage />} />
-      </Routes>
-    </Router>
-    // <div>Hello</div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/signup" element={<SignupPage />} />
+				<Route path="/login" element={<LoginPage />} />
+			</Routes>
+		</Router>
+		// <div>Hello</div>
+	);
 }
 
 export default App;
--- expense_tracker_frontend/src/pages/LoginPage.jsx
+++ expense_tracker_frontend/src/pages/LoginPage.jsx
@@ -3,12 +3,11 @@ import React, { useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-function SignupPage() {
+function LoginPage() {
 	const submitRef = useRef(null);
 	let navigate = useNavigate();
 
 	const [formData, setFormData] = useState({
-		name: "",
 		email: "",
 		password: "",
 	});
@@ -16,15 +15,15 @@ function SignupPage() {
 	async function handleSubmit(e) {
 		submitRef.current.disabled = true;
 		e.preventDefault();
-		if (!formData.name || !formData.email || !formData.password) {
+		if (!formData.email || !formData.password) {
 			alert("Please fill in all fields.");
 			return;
 		}
 		try {
-			await axios.post("http://localhost:8080/user/signUp", formData);
+			await axios.post("http://localhost:8080/user/login", formData);
 			navigate("/");
 		} catch (err) {
-			console.log(err);
+            console.log(err);
 			alert(err.response.data.error.toUpperCase());
 		} finally {
 			submitRef.current.disabled = false;
@@ -41,20 +40,9 @@ function SignupPage() {
 	return (
 		<div className="h-screen flex justify-center items-center bg-[#3081D0]">
 			<form className="flex flex-col text-xl items-center gap-4 bg-[#dfdd61] p-4 rounded-md  text-[#33689e]">
-				<div className="flex flex-col gap-1">
-					<p className="text-2xl font-bold underline w-full flex justify-center mb-4">
-						NEW USER
-					</p>
-					<label htmlFor="name">Username</label>
-					<input
-						className="bg-[#f5eec9] p-1 rounded-md"
-						type="text"
-						name="name"
-						required={true}
-						value={formData.name}
-						onChange={handleChange}
-					/>
-				</div>
+				<p className="text-2xl font-bold underline w-full flex justify-center mb-4">
+					WELCOME BACK !!!
+				</p>
 				<div className="flex flex-col gap-1">
 					<label htmlFor="name">Email</label>
 					<input
@@ -80,15 +68,15 @@ function SignupPage() {
 				<div className="bg-sky-400 hover:bg-sky-300 p-2 rounded-md">
 					<input
 						type="submit"
-						value="Sign Up"
+						value="Login"
 						ref={submitRef}
 						onClick={handleSubmit}
 					/>
 				</div>
 				<p>
-					Already have an account?{"  "}
-					<a className="underline" href="/login">
-						Login.
+					New User?{"  "}
+					<a className="underline" href="/signup">
+						Sign Up.
 					</a>
 				</p>
 			</form>
@@ -96,4 +84,4 @@ function SignupPage() {
 	);
 }
 
-export default SignupPage;
+export default LoginPage;
